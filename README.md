c43e50723c2a049397980cdfae5e0544c87a36d4
Samprikta Basu <basusam25@g.ucla.edu>
2022-10-05 17:17:21 -0700

    passes python tests

--- README.md
+++ README.md
@@ -1,22 +1,24 @@
 # A Kernel Seedling
 
-One sentence description
+Kernel module that displays the count or # of currently running processes.
 
 ## Building
 
-Explain how to build your kernel module
+Run the following commands:
+make
+sudo insmod proc_count.ko
 
 ## Running
 
-Explain how to run your kernel module and what to expect
+Run the kernel module with the following command: cat /proc/count
+Expect an integer output that represents the # of running processes.
 
 ## Cleaning Up
 
-Explain how to remove your kernel module and clean up the code
+Remove the kernel module with the following command: sudo rmmod proc_count.ko
+
+Inside the code, clean up is performed using the function proc_remove() in the kernel module's exit function to clean up the directory that was created by proc_create_single upon init. 
 
 ## Testing
 
-Report which kernel release version you tested your module on
-(hint: use `uname`, check for options with `man uname`).
-It should match release numbers as seen on https://www.kernel.org/.
-
+Kernel Release Version: 5.14.8-arch1-1
--- proc_count.c
+++ proc_count.c
@@ -4,20 +4,44 @@
 #include <linux/seq_file.h>
 #include <linux/sched.h>
 
+// global var to store directory
+static struct proc_dir_entry *pc;
+
+static int get_proc_count(void) {
+  int counter = 0;
+  struct task_struct *t;
+  
+  // loop through processes and increment counter
+  for_each_process(t) {
+    counter++;
+  }
+  return counter;
+}
+
+// write # of processes count to "count" file
+static int counting_show(struct seq_file *s, void *v) {
+  seq_printf(s, "%d\n", get_proc_count());
+  return 0;
+}
+
 static int __init proc_count_init(void)
 {
 	pr_info("proc_count: init\n");
+	// create proc entry that "shows" the processes count
+	pc = proc_create_single("count", 0, NULL, counting_show);
 	return 0;
 }
 
 static void __exit proc_count_exit(void)
-{
+{        
 	pr_info("proc_count: exit\n");
+	// clean up the directory
+	proc_remove(pc);
 }
 
 module_init(proc_count_init);
 module_exit(proc_count_exit);
 
-MODULE_AUTHOR("Your name");
-MODULE_DESCRIPTION("One sentence description");
+MODULE_AUTHOR("Samprikta Basu");
+MODULE_DESCRIPTION("Creates /proc/count file displaying count of currently running processes.");
 MODULE_LICENSE("GPL");
